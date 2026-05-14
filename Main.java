cacc65e0123fc50233ad1f64dff26d2ad226e964
Viturico <albyrg@gmail.com>
2026-05-14 15:24:22 +0100

    Se añade la palabra 'Euros' la mensaje del saldo actual

--- Main.java
+++ Main.java
@@ -7,10 +7,10 @@ public class Main {
         double saldoActual;
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es " + saldoActual);
+        System.out.println("El saldo actual es " + saldoActual + " Euros");
         operaciones(cuenta1);
         saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es " + saldoActual);
+        System.out.println("El saldo actual es " + saldoActual + " Euros");
     }
 
     private static void operaciones(CCuenta cuenta1) {
